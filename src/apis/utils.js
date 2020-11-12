343c255658945274cfd679e44324099e731ce94c
dannnyliang <youchenliang@gmail.com>
2020-11-12 22:59:43 +0800

    create `isProd`, `getEndpoint` util functions

---
+++ src/apis/utils.js
@@ -0,0 +1,8 @@
+import { isProd } from "../utils";
+
+export function getEndpoint(functionName) {
+  const { VUE_APP_API_PORT, VUE_APP_FIREBASE_REGION, VUE_APP_FIREBASE_PROJECT_NAME } = process.env;
+  return isProd()
+    ? `https://${VUE_APP_FIREBASE_REGION}-${VUE_APP_FIREBASE_PROJECT_NAME}.cloudfunctions.net/${functionName}`
+    : `http://localhost:${VUE_APP_API_PORT}/${VUE_APP_FIREBASE_PROJECT_NAME}/${VUE_APP_FIREBASE_REGION}/${functionName}`;
+}
