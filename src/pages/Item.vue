c39f6c3c81d34513c2b1d3af79cd0be89391cf4a
dannnyliang <youchenliang@gmail.com>
2020-11-14 17:06:06 +0800

    use FormGame and query game data when Item page created

--- src/pages/Item.vue
+++ src/pages/Item.vue
@@ -1,20 +1,43 @@
 <template>
-  <h1>Item Page</h1>
+  <h1 @click="log">Item Page</h1>
   <p>Item id: {{ $route.params.id }}</p>
-  <router-link class="back" to="/list">Back to list</router-link>
+  <FormGame :initialValues="data" @submit="create" />
+  <router-link to="/list">
+    <div class="back">Back to list</div>
+  </router-link>
 </template>
 
 <script>
+import FormGame from "../components/FormGame";
+import getGame from "../apis/getGame";
+
 export default {
   name: "Item",
+  components: {
+    FormGame,
+  },
+  data() {
+    return {
+      loading: true,
+      data: {},
+    };
+  },
+  async created() {
+    const data = await getGame(this.$route.params.id);
+    this.$data.data = data;
+    this.$data.loading = false;
+  },
 };
 </script>
 
 <style>
+a {
+  text-decoration: none;
+}
 .back {
   color: #42b983;
-  text-decoration: none;
   font-weight: bold;
   font-size: 20px;
+  margin-top: 16px;
 }
 </style>
