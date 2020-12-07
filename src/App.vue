95127492356b58f55db92815bd7adcd97ba40300
dannnyliang <youchenliang@gmail.com>
2020-12-08 01:23:12 +0800

    build basic app layout

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,87 @@
 <template>
-  <div class="links">
-    <router-link to="/">Home</router-link>
-    <router-link to="/list">List</router-link>
-    <router-link to="/create">Create</router-link>
-  </div>
+  <a-layout class="navi">
+    <a-layout-header class="header">
+      <router-link to="/">
+        <div class="logo" />
+      </router-link>
+      <a-menu
+        class="header-menu"
+        mode="horizontal"
+        v-model:selectedKeys="selectedKeys"
+      >
+        <a-menu-item key="list">
+          <router-link to="/list">列表</router-link>
+        </a-menu-item>
+        <a-menu-item key="create">
+          <router-link to="/create">新增</router-link>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-header>
 
-  <router-view> </router-view>
+    <a-layout-content class="content">
+      <router-view> </router-view>
+    </a-layout-content>
+  </a-layout>
 </template>
 
 <script>
+import { Layout, Menu } from "ant-design-vue";
+import { router } from "./main";
+
 export default {
   name: "App",
+  components: {
+    ALayout: Layout,
+    ALayoutHeader: Layout.Header,
+    ALayoutContent: Layout.Content,
+    AMenu: Menu,
+    AMenuItem: Menu.Item,
+  },
+  computed: {
+    selectedKeys() {
+      const routeName = router.resolve(window.location).name;
+      return [routeName];
+    },
+  },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 40px;
+<style scoped>
+.navi {
+  --headerHeight: calc(var(--l) * 4);
 }
 
-.links a {
-  color: #42b983;
-  text-decoration: none;
-  font-weight: bold;
-  font-size: 20px;
-  margin-right: 8px;
+.ant-layout {
+  min-height: 100vh;
 }
-.links a:last-child {
-  margin-right: 0;
+
+.header {
+  display: flex;
+  align-items: center;
+
+  position: fixed;
+  z-index: 1;
+  width: 100%;
+
+  background-color: var(--white);
 }
-.links a:hover {
-  text-decoration: underline;
+
+.logo {
+  width: calc(var(--l) * 3);
+  height: calc(var(--l) * 3);
+  background-size: contain;
+  background-image: url("./assets/logo.jpg");
+  border-radius: 50%;
+  float: left;
+  margin-right: calc(var(--l) * 2);
+}
+
+.header-menu {
+  line-height: var(--headerHeight);
+}
+
+.content {
+  margin-top: var(--headerHeight);
+  padding: var(--xl);
 }
 </style>
