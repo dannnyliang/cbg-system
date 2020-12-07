cd344d40c40f031cb56a4476796af38afbf09052
dannnyliang <youchenliang@gmail.com>
2020-12-08 01:22:35 +0800

    overwrite ant-design-vue global style

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          modifyVars: {
+            "primary-color": "#42b983",
+            "link-color": "#42b983",
+            "font-size-base": "16px",
+          },
+          javascriptEnabled: true,
+        },
+      },
+    },
+  },
+};
