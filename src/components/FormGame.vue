1bcb739d859c8c141e5172d2b490084f2101084c
dannnyliang <youchenliang@gmail.com>
2020-12-10 01:36:19 +0800

    use `form` on FormGame

--- src/components/FormGame.vue
+++ src/components/FormGame.vue
@@ -1,74 +1,76 @@
 <template>
-  <form>
-    <p>
-      <label for="name">Name: </label>
-      <input type="text" name="name" id="name" v-model="form.name" />
-    </p>
+  <a-form :model="form" :label-col="labelCol" :wrapper-col="wrapperCol">
+    <a-form-item label="名稱（中文）">
+      <a-input v-model:value="form.name" />
+    </a-form-item>
 
-    <p>
-      <label for="minPlayers">minPlayers: </label>
-      <input
-        type="number"
-        name="minPlayers"
-        id="minPlayers"
-        v-model="form.minPlayers"
-        min="0"
-      />
-    </p>
+    <a-form-item label="最少人數">
+      <a-input-number v-model:value="form.minPlayers" :min="1" />
+    </a-form-item>
 
-    <p>
-      <label for="maxPlayers">maxPlayers: </label>
-      <input
-        type="number"
-        name="maxPlayers"
-        id="maxPlayers"
-        v-model="form.maxPlayers"
-        min="0"
-      />
-    </p>
+    <a-form-item label="最多人數">
+      <a-input-number v-model:value="form.maxPlayers" :min="1" />
+    </a-form-item>
 
-    <label for="coverImage">Cover Image: </label>
-    <p v-if="$props?.initialValues?.coverImage">
-      <img :src="$props?.initialValues?.coverImage" width="100" height="100" />
-    </p>
-    <p v-else>
+    <a-form-item label="封面">
       <a-upload
-        name="coverImage"
-        id="coverImage"
         accept="image/*"
         list-type="picture-card"
-        :action="uploadUrl"
+        v-model:fileList="form.coverImage"
         :multiple="true"
-        :remove="handleCoverRemove"
+        :action="uploadUrl"
         @change="handleCoverChange"
-        @preview="handlePreview"
+        @preview="handlePreview($event?.response?.url)"
       >
-        <a-button> Click to Upload </a-button>
+        <div v-if="showCoverImageUploadButton">
+          <loading-outlined v-if="loading" />
+          <div v-else>
+            <plus-outlined />
+            <div>上傳</div>
+          </div>
+        </div>
       </a-upload>
-      <a-modal :visible="previewVisible" :footer="null" @cancel="handleCancel">
-        <img style="width: 100%" :src="previewImage" />
-      </a-modal>
-    </p>
+    </a-form-item>
+    <a-modal :visible="previewVisible" :footer="null" @cancel="handleCancel">
+      <img style="width: 100%" :src="previewImage" />
+    </a-modal>
 
-    <p>
-      <label for="type">Game type: </label>
-      <select name="type" id="type" v-model="form.type">
-        <option v-for="(label, key) in $GAME_TYPE" :key="key" :value="key">
+    <a-form-item label="類別">
+      <a-select v-model:value="form.type">
+        <a-select-option
+          v-for="(label, key) in $GAME_TYPE"
+          :key="key"
+          :value="key"
+        >
           {{ label }}
-        </option>
-      </select>
-    </p>
+        </a-select-option>
+      </a-select>
+    </a-form-item>
 
-    <button @click.prevent="$emit('submit', payloadTransformer($data.form))">
-      submit
-    </button>
-  </form>
+    <a-form-item :wrapper-col="{ offset: 4 }">
+      <a-button
+        type="primary"
+        @click="$emit('submit', payloadTransformer($data.form))"
+      >
+        Create
+      </a-button>
+    </a-form-item>
+  </a-form>
 </template>
 
 <script>
-import { Upload, Button, Modal } from "ant-design-vue";
-import removeFile from "../apis/removeFile";
+import {
+  Upload,
+  Button,
+  Modal,
+  Form,
+  Input,
+  InputNumber,
+  Select,
+} from "ant-design-vue";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons-vue";
 import { getUploadFileEndpoint } from "../apis/utils";
+import { getFileListFromFileInfo } from "../utils";
 
 export default {
   name: "FormGame",
@@ -77,21 +79,34 @@ export default {
     AUpload: Upload,
     AButton: Button,
     AModal: Modal,
+    AForm: Form,
+    AFormItem: Form.Item,
+    AInput: Input,
+    AInputNumber: InputNumber,
+    ASelect: Select,
+    ASelectOption: Select.Option,
+    LoadingOutlined,
+    PlusOutlined,
   },
   computed: {
     uploadUrl() {
       return getUploadFileEndpoint("coverImages");
     },
+    showCoverImageUploadButton() {
+      return this.$data.form.coverImage?.length < 1;
+    },
   },
   data() {
     return {
+      labelCol: { span: 4 },
+      wrapperCol: { span: 12 },
+      loading: false,
       previewVisible: false,
       previewImage: "",
       form: {},
     };
   },
   mounted() {
-    console.log(this.$props);
     this.initForm(this.initialValues);
   },
   watch: {
@@ -101,15 +116,18 @@ export default {
   },
   methods: {
     initForm(initialValues) {
-      if (initialValues?.coverImage) {
-        this.$data.defaultCoverImage = [{ url: initialValues?.coverImage }];
-      }
       this.$data.form = {
         name: initialValues?.name,
         minPlayers: initialValues?.minPlayers ?? 1,
         maxPlayers: initialValues?.maxPlayers ?? 1,
         type: initialValues?.type,
-        coverImage: initialValues?.coverImage,
+        coverImage: initialValues?.coverImage
+          ? getFileListFromFileInfo([
+              {
+                url: initialValues?.coverImage,
+              },
+            ])
+          : [],
       };
     },
     payloadTransformer(formValues) {
@@ -117,36 +135,32 @@ export default {
         ...formValues,
         minPlayers: Number(formValues.minPlayers),
         maxPlayers: Number(formValues.maxPlayers),
+        coverImage: formValues.coverImage?.[0]?.response?.url ?? null,
       };
     },
+
+    // upload
     handleCancel() {
       this.previewVisible = false;
     },
-    handleCoverChange(event) {
-      const { file } = event;
-      if (file.status === "done") {
-        this.$data.form.coverImage = file.response.url;
-      }
-    },
-    async handlePreview(file) {
-      if (!file?.response?.url) {
+    async handlePreview(imageUrl) {
+      if (!imageUrl) {
         return;
       }
-      this.previewImage = file.response.url;
+      this.previewImage = imageUrl;
       this.previewVisible = true;
     },
-    handleCoverRemove(event) {
-      this.$data.form.coverImage = null;
-      return removeFile(event.response.filePath);
+    handleCoverChange(event) {
+      const { file, fileList } = event;
+
+      this.$data.form.coverImage = fileList;
+      if (file.status === "loading") {
+        this.$data.loading = true;
+      }
+      if (file.status === "done") {
+        this.$data.loading = false;
+      }
     },
   },
 };
 </script>
-
-<style scoped>
-.ant-upload-picture-card-wrapper {
-  text-align: initial;
-  display: flex;
-  justify-content: center;
-}
-</style>
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,15 @@
 export function isProd() {
-  return process.env.NODE_ENV === 'production'
+  return process.env.NODE_ENV === "production";
+}
+
+export function getFileListFromFileInfo(fileInfoList) {
+  return fileInfoList.map((fileInfo, index) => ({
+    uid: `${index}`,
+    name: fileInfo.name ?? "",
+    status: "done",
+    url: fileInfo.url,
+    response: {
+      url: fileInfo.url,
+    },
+  }));
 }
