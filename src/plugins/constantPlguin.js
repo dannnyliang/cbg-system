a06f5a779c8432fdc953784a3ab79c2962d2f218
dannnyliang <youchenliang@gmail.com>
2021-02-09 16:14:16 +0800

    initialize with create-next-app

--- src/plugins/constantPlguin.js
+++
@@ -1,9 +0,0 @@
-import * as constants from '../constants'
-
-export default {
-  install: (app) => {
-    Object.keys(constants).forEach(key => {
-      app.config.globalProperties[`$${key}`] = constants[key];
-    })
-  },
-};
